9e255e7567183de39922d315d996ba959a4ead06
github username <github email address>
2017-09-08 11:11:00 -0400

    Done.

--- author.rb
+++ author.rb
@@ -1,1 +1,38 @@
+class Author
 
+  attr_accessor :author
+
+  @@authors
+
+  def initialize(author)
+    @author = author
+    @@authors << author
+    @books = []
+  end
+
+  def books
+    @author.collect do |author|
+      author.book
+    end
+  end
+
+  def write_book(title, count)
+    book.author = self
+    book.title = title
+  end
+
+  def total_words
+    @author.collect.split.size do |author|
+      author.book
+    end
+  end
+
+  def most_words
+    
+  end
+
+  def self.all
+    @@authors
+  end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,30 @@
+class Book
 
+  attr_accessor :book, :author, :title
+
+  @@books = []
+
+  def initialize(book, author, title)
+    @book = book
+    @author = author
+    @title = title
+    @@books << book
+  end
+
+  def self.all
+    @@books
+  end
+
+  def author(book)
+    @book.author
+  end
+
+  def title(book)
+    @book.title
+  end
+
+  def word_count(book)
+    count = @book.split.size
+  end
+
+end
